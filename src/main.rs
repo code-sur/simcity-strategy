99a04c56488fd270543af9eb702b98bd57bd2aee
Code Raguet <iraguet@w3itsolutions.net>
2024-11-01 14:53:22 -0300

    Adds stub solve function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[derive(Debug, PartialEq)]
+struct Solution {}
+
+struct Fabric {}
+
+fn solve(available_factories: Vec<Fabric>) -> Solution {
+    Solution {}
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{solve, Fabric, Solution};
+
+    #[test]
+    fn test() {
+        let available_factories: Vec<Fabric> = Vec::new();
+        let minimal_solution: Solution = Solution {};
+        assert_eq!(solve(available_factories), minimal_solution);
+    }
+}
