68eeb6ee35c86dbf91e92495536929fd978fa760
Code Raguet <iraguet@w3itsolutions.net>
2024-11-14 14:59:17 -0300

    Restarts project. Example solution driven

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
-fn main() {
-    println!("Hello, world!");
-}
-
-#[derive(Debug, PartialEq)]
 struct Solution {}
-
-struct Fabric {}
-
-fn solve(available_factories: Vec<Fabric>) -> Solution {
-    Solution {}
+impl Solution {
+    fn value(&self) -> Simoleons {
+        Simoleons(50)
+    }
 }
+#[derive(Debug, PartialEq)]
+struct Simoleons(usize);
+
+fn main() {}
 
 #[cfg(test)]
 mod tests {
-    use crate::{solve, Fabric, Solution};
+    use crate::{Simoleons, Solution};
 
     #[test]
-    fn test() {
-        let available_factories: Vec<Fabric> = Vec::new();
-        let minimal_solution: Solution = Solution {};
-        assert_eq!(solve(available_factories), minimal_solution);
+    fn metal_solution() {
+        let solution: Solution = Solution {};
+        let five_metal_value: Simoleons = Simoleons(50);
+        assert_eq!(solution.value(), five_metal_value);
     }
 }
